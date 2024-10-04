94dfef9e72bc520e3d8da06176c682848d6a5256
Himanshu Verma <himanshuv309@gmail.com>
2024-10-04 23:14:52 +0530

    background change

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Technologies from "./components/Technologies";
 
 function App() {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 min-h-full w-full"></div>
-      <div className="absolute top-0 z-[-1] h-full w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%, rgba(120,119,198,0.3), rgba(255,255,255,0))] min-h-screen"></div>
-      <div className="container mx-auto px-8">
+    <div className="relative overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      {/* Background applied to a parent container */}
+        
+      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      {/* Main container for the content */}
+      <div className="min-h-screen container mx-auto px-8">
         <Navbar />
         <Hero />
         <About />
