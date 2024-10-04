36535237f07ca5a2029380a53354d4fc055b2154
Himanshu Verma <himanshuv309@gmail.com>
2024-10-04 14:35:02 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
-  
-
   return (
-   
-      <div className="overflow-x-hidden text-neutral-300 antialiased
-                      Iselection:bg-cyan-300 ☐ selection: text-cyan-900">
-      <div className="flixed top-0 -z-10 h-full w-full"></div>
-      <div className="absolute top-0 z-[-2] h-screen w-screen ☐ bg-neutral-950 bg-
-                      [radial-gradient(ellipse_80%_80%_at_50%-20%, rgba(120,119,198,0.3), rgba(255,
-                        255,255,0))]"></div>
-      <div className="container mx-auto px-8"></div>
-      <Navbar/>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="fixed top-0 -z-10 h-screen w-full"></div>
+      <div className="absolute top-0 z-[-1] h-full w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%, rgba(120,119,198,0.3), rgba(255,255,255,0))] min-h-screen"></div>
+      <div className="container mx-auto px-8">
+        <Navbar />
+        <Hero />
       </div>
-      
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
