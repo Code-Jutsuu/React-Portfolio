63ce4375df37d2be12bd7764c9130bc9d4f0ec5a
Himanshu Verma <himanshuv309@gmail.com>
2024-10-06 16:00:55 +0530

    background

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className="relative overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       {/* Background applied to a parent container */}
         
-      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_50%,#63e_100%)]"></div>
       {/* Main container for the content */}
       <div className="min-h-screen container mx-auto px-8">
         <Navbar />
